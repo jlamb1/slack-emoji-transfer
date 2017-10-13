c9b4284678dc2a697d27e643338cde3ffd4c853a
John <lambj71@gmail.com>
2017-10-13 17:59:22 -0400

    added auto convert to yaml output

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+"use strict";
 const diff = require('deep-diff');
 const util = require('util');
 const fs = require('fs');
+let YAML = require('json2yaml'), ymlText;
+
 
 let olddata = fs.readFileSync('old.json');
 let old = JSON.parse(olddata); 
@@ -8,16 +11,24 @@ let old = JSON.parse(olddata);
 let newdata = fs.readFileSync('new.json');
 let new1 = JSON.parse(newdata);
 
-
 let differences = diff(old, new1);
-
 let diffCheck = JSON.stringify(differences, null, 4);
-
 
 fs.writeFile("output.json", diffCheck, function(err) {
     if(err) {
         return console.log(err);
     }
-
     console.log("These are your missing emojis");
+});
+
+let dedup = fs.readFileSync('old.json');
+let output = JSON.parse(dedup);
+
+ymlText = YAML.stringify(output);
+
+fs.writeFile("output.yaml", ymlText, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("in yaml output");
 });
